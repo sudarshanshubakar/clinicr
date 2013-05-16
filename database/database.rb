a6b41416ce75dfc642b7da74d9be8a72d95b28bd
sudarshanshubakar <sudarshanshubakar@gmail.com>
2013-05-16 06:15:48 +0530

    Intermediate commit. Database operations changed to use user id. High level functions to be changed.

--- database/database.rb
+++ database/database.rb
@@ -6,16 +6,17 @@ class Database_redis
     @redis = Redis.new
   end
 
-  def update_patient(id, details_hash)
-    puts "ID = #{id}"
+  def update_patient(user_id, patient_id, details_hash)
+    puts "patient_id = #{patient_id}"
+    puts "user_id = #{user_id}"
     puts "details = #{details_hash}"
-    update_patient_details(id, details_hash)
+    update_patient_details(user_id, patient_id, details_hash)
   end
   
-  def find_patient(criteria)
-    result = find_details(criteria)# get_result_from_details(criteria_string)
+  def find_patient(user_id, criteria)
+    result = find_details(user_id, criteria)# get_result_from_details(criteria_string)
     if (result.empty?)
-      result = find_reference(criteria)
+      result = find_reference(user_id, criteria)
     end
     puts "RESULT == #{result}"
     return result
@@ -23,51 +24,51 @@ class Database_redis
 
 
 
-  def get_patient_details(id)
-    details = @redis.hget("patient:details", id)
+  def get_patient_details(user_id, patient_id)
+    details = @redis.hget("#{user_id}:patient:details", patient_id)
     return details
   end
 
-  def get_visit_history(id)
-    history_references = @redis.zrevrange("visit_history:#{id}", 0, -1)
-    history_entries = get_history_entries(id, history_references)
+  def get_visit_history(user_id, patient_id)
+    history_references = @redis.zrevrange("#{user_id}:visit_history:#{patient_id}", 0, -1)
+    history_entries = get_history_entries(history_references)
 
   end
 
-  def add_patient(id, details_hash)
-    update_patient_details(id, details_hash)
+  def add_patient(user_id, patient_id, details_hash)
+    update_patient_details(user_id, patient_id, details_hash)
   end
   
-  def update_patient_details(id, details_hash)
+  def update_patient_details(user_id, patient_id, details_hash)
     details_entry = hash_to_json_string(details_hash)
     name = details_hash["name"]
     contact = details_hash["contact"]
-    @redis.hset("patient:details", id, details_entry)
-    @redis.hset("patient:reference", name, id)
-    @redis.hset("patient:reference", contact, id)    
+    @redis.hset("#{user_id}:patient:details", patient_id, details_entry)
+    @redis.hset("#{user_id}:patient:reference", name, patient_id)
+    @redis.hset("#{user_id}:patient:reference", contact, patient_id)    
   end
 
-  def add_history_entry(id, timestamp, history_entry)
-    history_key = "visit_history:#{id}:#{timestamp}"
+  def add_history_entry(user_id, patient_id, timestamp, history_entry)
+    history_key = "#{user_id}:visit_history:#{patient_id}:#{timestamp}"
     @redis.set(history_key, history_entry)
-    patient_history_key = "visit_history:#{id}"
+    patient_history_key = "#{user_id}:visit_history:#{patient_id}"
     weight = @redis.zcount(patient_history_key, "-inf", "+inf") + 1
     @redis.zadd(patient_history_key, weight, history_key)
   end
 
   # Low level function. Not recommended for use from higher level functions. Use generate_id in Auto_generator(autogenerator.rb) instead.
-  def generate_id
-    cur_value = @redis.get("id_gen:patient:current_value")
+  def generate_id(user_id)
+    cur_value = @redis.get("#{user_id}:id_gen:patient:current_value")
     id = cur_value.to_i + 1
-    @redis.set("id_gen:patient:current_value", id)
+    @redis.set("#{user_id}:id_gen:patient:current_value", id)
     return id
   end
 
   private
-  def get_history_entries(id, history_references)
+  def get_history_entries(history_references)
     history_entries = []
     history_references.each do |hist_ref_id|
-      date = hist_ref_id.split(":")[2]
+      date = hist_ref_id.split(":")[3]
       history_value = @redis.get(hist_ref_id)
       history_entries << "#{date}||#{history_value}"
     end
--- database/helpers/finders.rb
+++ database/helpers/finders.rb
@@ -1,6 +1,6 @@
 module Finder
-  def get_matching_keys(location, criteria)
-    details_keys = @redis.hkeys("patient:#{location}")
+  def get_matching_keys(user_id, location, criteria)
+    details_keys = @redis.hkeys("#{user_id}:patient:#{location}")
     matching_keys = details_keys.grep(/^#{criteria}/)
   end
 end
@@ -9,11 +9,11 @@ end
 module Detail_finder 
   include Finder
 
-  def find_details(criteria_string)
-    matching_keys = get_matching_keys("details", criteria_string)
+  def find_details(user_id, criteria_string)
+    matching_keys = get_matching_keys("details", user_id, criteria_string)
     matching_values = []
     matching_keys.each do |key|
-      value = @redis.hget("patient:details", key)
+      value = @redis.hget("#{user_id}:patient:details", key)
       puts "#{key} === #{value}"
       hash_result = JSON.parse(value)
       hash_result["id"] = key
@@ -25,12 +25,12 @@ end
 
 module Reference_finder
   include Finder
-  def find_reference(criteria_string)
-    matching_keys = get_matching_keys("reference", criteria_string)
+  def find_reference(user_id, criteria_string)
+    matching_keys = get_matching_keys("reference", user_id, criteria_string)
     matching_values = []
     matching_keys.each do |key|
-      patient_id = @redis.hget("patient:reference", key)
-      details = @redis.hget("patient:details", patient_id)
+      patient_id = @redis.hget("#{user_id}:patient:reference", key)
+      details = @redis.hget("#{user_id}:patient:details", patient_id)
       hash_result = JSON.parse(details)
       hash_result["id"] = patient_id
       matching_values << hash_result
