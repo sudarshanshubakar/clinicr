afb766a6eee8121f091fc9dea554225ee48a1d3c
Sudarshan Shubakar <sudarshanshubakar@gmail.com>
2013-07-10 06:44:16 +0530

    Fixed Bug in clinicr.js resulting in double update. And improved the overlay experience over slow connections.

--- public/js/clinicr.js
+++ public/js/clinicr.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  
+
 
 
   $("#search").submit(function(submitEvent) {
@@ -9,9 +9,9 @@ $(document).ready(function() {
     postAndAddToLocation_search_result(url, formValues, "#search_result_area");
 
     var pat_bas_info_area =  $(document).find("#patient_basic_information_section");
-    $(pat_bas_info_area).css("display", "none");    
+    $(pat_bas_info_area).css("display", "none");
     var visit_history_section =  $(document).find("#visit_history_section");
-    $(visit_history_section).css("display", "none");       
+    $(visit_history_section).css("display", "none");
   });
 
   $("#update_form").submit(function(submitEvent) {
@@ -20,173 +20,165 @@ $(document).ready(function() {
     var url = $(this).attr("action");
     var location = $(this).attr("location");
     var formValues = $(this).serialize();
-    
+
     $.post(url, formValues, function(returnHTML) {
-      
+
       details_url = "/details/getDetails?id="+id;
       history_url = "/visitHistory/getVisitHistory?id="+id;
       getAndAddToLocation_callback(function(){
         getAndAddToLocation(history_url, "#visit_history_section");
-      }, details_url, "#patient_basic_information_section");
-      hideOverlay();
-    });    
-    
-    
-    
-    
-    postAndAddToLocation(url, formValues, location);
-    visit_url = "/visitHistory/getVisitHistory?id="+id;
-    getAndAddToLocation(visit_url, "#visit_history_section");
-    setTimeout(100);
-    hideOverlay();
-  });
-
-  $("#add_patient_form").submit(function(submitEvent) {
-    submitEvent.preventDefault();
-    var url = $(this).attr("action");
-    var formValues = $(this).serialize();
-    var id = "";
-    var search_result_area =  $(document).find("#search_result_area");
-    $(search_result_area).css("display", "none");
-    $.post(url, formValues, function(returnHTML) {
-      id = returnHTML;
-      details_url = "/details/getDetails?id="+id;
-      history_url = "/visitHistory/getVisitHistory?id="+id;
-      getAndAddToLocation_callback(function(){
-        getAndAddToLocation(history_url, "#visit_history_section");
-      }, details_url, "#patient_basic_information_section");
-      hideOverlay();
+        }, details_url, "#patient_basic_information_section");
+        hideOverlay();
+      });
     });
-  });
-  
-  $("#update_patient_details_form").submit(function(submitEvent) {
-    submitEvent.preventDefault();
-    var url = $(this).attr("action");
-    var formValues = $(this).serialize();
-    var id = "";
-    $.post(url, formValues, function(returnHTML) {
-      id = returnHTML;
-      details_url = "/details/getDetails?id="+id;
-      getAndAddToLocation(details_url, "#patient_basic_information_section");
-    });
-  });
 
-  $('body').on('click', '#links a',function(event){
-    event.preventDefault();
-    var url = $(this).attr("href");
-    getAndOverlay(url);
-  });
-
-
-  $('body').on('click', '#search_result_hyperlink a',function(event){
-    event.preventDefault();
-    var url = $(this).attr("href");
-    getAndAddToLocation_callback(function(url) {
-      var params = url.split("?")[1];
-      var each_param = params.split("&")
+    $("#add_patient_form").submit(function(submitEvent) {
+      submitEvent.preventDefault();
+      var url = $(this).attr("action");
+      var formValues = $(this).serialize();
       var id = "";
-      for (var i=0, len=each_param.length; i < len; i++) {
-        var kv_pair = each_param[i].split("=");
-        var key = kv_pair[0];
-        if (key == "id") {
-          id = kv_pair[1];
-          break;
-        }
-      }
-      var url = "/visitHistory/getVisitHistory?id="+id;
-      getAndAddToLocation(url, "#visit_history_section");
-    },url, "#patient_basic_information_section");
-    var search_result_area =  $(document).find("#search_result_area");
-    $(search_result_area).css("display", "none");
-  });
-
-  $('body').on('click', '#show_history_hyperlink a',function(event){
-    event.preventDefault();
-    var url = $(this).attr("href");
-    var location = $(this).attr("location");
-    getAndAddToLocation(url, location);
-  });
-  
-  $('body').on('click', '#cancel_update_patient_details' ,function(event){
-    event.preventDefault();
-    var url = $(this).attr("href");
-    getAndAddToLocation(url, "#patient_basic_information_section");
-  });
-  
-  $('body').on('click', '#details_hyperlinks a',function(event){
-    event.preventDefault();
-    var url = $(this).attr("href");
-    var location = $(this).attr("location");
-    getAndAddToLocation(url, location);
-  });
-
-  $('body').on('click', '#show_new_visit_form a',function(event){
-    event.preventDefault();
-    var url = $(this).attr("href");
-    getAndOverlay(url);    
-  });
-  
-  function getAndOverlay(url) {
-    showOverlay();
-      $.get(url, function(returnHTML) {
-        var overlay_location = $(document).find("#overlayArea");
-        var result_location = $(document).find("#overlayContent");
-        $(result_location).html(returnHTML);
-        $(overlay_location).css("opacity", "0.7");
-
-        $(result_location).css("opacity", "1");
-        //showOverlay();
-      });    
-  }
-  
-  function showOverlay() {
-    var overlay_location = $(document).find("#overlayArea");
-    var result_location = $(document).find("#overlayContent");  
-    $(overlay_location).css("display", "block");
-    $(result_location).css("display", "block");       
-  }
-  
-
-    function postAndAddToLocation(url, formValues, location) {
-      $.post(url, formValues,
-        function(returnHTML) {
-          addToLocation(returnHTML,location);
+      var search_result_area =  $(document).find("#search_result_area");
+      $(search_result_area).css("display", "none");
+      $.post(url, formValues, function(returnHTML) {
+        id = returnHTML;
+        details_url = "/details/getDetails?id="+id;
+        history_url = "/visitHistory/getVisitHistory?id="+id;
+        getAndAddToLocation_callback(function(){
+          getAndAddToLocation(history_url, "#visit_history_section");
+          }, details_url, "#patient_basic_information_section");
+          hideOverlay();
         });
-      }
-      
-      function postAndAddToLocation_search_result(url, formValues, location) {
-      $.post(url, formValues,
-        function(returnHTML) {
-          addToLocation(returnHTML,location);
+      });
+
+      $("#update_patient_details_form").submit(function(submitEvent) {
+        submitEvent.preventDefault();
+        var url = $(this).attr("action");
+        var formValues = $(this).serialize();
+        var id = "";
+        $.post(url, formValues, function(returnHTML) {
+          id = returnHTML;
+          details_url = "/details/getDetails?id="+id;
+          getAndAddToLocation(details_url, "#patient_basic_information_section");
         });
-      } 
-      
-      function getAndAddToLocation_callback(callback, url, location ) {
-        getAndAddToLocation(url, location);
-        if (typeof callback === "function") {
-          callback(url);
+      });
+
+      $('body').on('click', '#links a',function(event){
+        event.preventDefault();
+        var url = $(this).attr("href");
+        getAndOverlay(url);
+      });
+
+
+      $('body').on('click', '#search_result_hyperlink a',function(event){
+        event.preventDefault();
+        var url = $(this).attr("href");
+        getAndAddToLocation_callback(function(url) {
+          var params = url.split("?")[1];
+          var each_param = params.split("&")
+          var id = "";
+          for (var i=0, len=each_param.length; i < len; i++) {
+            var kv_pair = each_param[i].split("=");
+            var key = kv_pair[0];
+            if (key == "id") {
+              id = kv_pair[1];
+              break;
+            }
+          }
+          var url = "/visitHistory/getVisitHistory?id="+id;
+          getAndAddToLocation(url, "#visit_history_section");
+          },url, "#patient_basic_information_section");
+          var search_result_area =  $(document).find("#search_result_area");
+          $(search_result_area).css("display", "none");
+        });
+
+        $('body').on('click', '#show_history_hyperlink a',function(event){
+          event.preventDefault();
+          var url = $(this).attr("href");
+          var location = $(this).attr("location");
+          getAndAddToLocation(url, location);
+        });
+
+        $('body').on('click', '#cancel_update_patient_details' ,function(event){
+          event.preventDefault();
+          var url = $(this).attr("href");
+          getAndAddToLocation(url, "#patient_basic_information_section");
+        });
+
+        $('body').on('click', '#details_hyperlinks a',function(event){
+          event.preventDefault();
+          var url = $(this).attr("href");
+          var location = $(this).attr("location");
+          getAndAddToLocation(url, location);
+        });
+
+        $('body').on('click', '#show_new_visit_form a',function(event){
+          event.preventDefault();
+          var url = $(this).attr("href");
+          getAndOverlay(url);
+        });
+
+        function getAndOverlay(url) {
+          showOverlay();
+          $(overlay_location).css("opacity", "0.7");
+          $(result_location).css("opacity", "1");
+
+          $.get(url, function(returnHTML) {
+            var overlay_location = $(document).find("#overlayArea");
+            var result_location = $(document).find("#overlayContent");
+            $(result_location).html(returnHTML);
+
+            //showOverlay();
+          });
         }
-      }
-      
-      function getAndAddToLocation(url, location) {
-        $.get(url, function(returnHTML) {
-          addToLocation(returnHTML, location);
-        });        
-      }
 
-      function addToLocation(html, location) {
-        var result_location = $(document).find(location);
-        $(result_location).html(html);
-        $(result_location).css("display", "block");
-      }
+        function showOverlay() {
+          var overlay_location = $(document).find("#overlayArea");
+          var result_location = $(document).find("#overlayContent");
+          $(overlay_location).css("display", "block");
+          $(result_location).css("display", "block");
+        }
 
-    });
-    
-      function hideOverlay() {
-    var overlay_location = $(document).find("#overlayArea");
-    var result_location = $(document).find("#overlayContent");
-    $(result_location).html(""); 
-    $(overlay_location).css("display", "none");
-    $(result_location).css("display", "none");     
-  }
+
+        function postAndAddToLocation(url, formValues, location) {
+          $.post(url, formValues,
+            function(returnHTML) {
+              addToLocation(returnHTML,location);
+            });
+          }
+
+          function postAndAddToLocation_search_result(url, formValues, location) {
+            $.post(url, formValues,
+              function(returnHTML) {
+                addToLocation(returnHTML,location);
+              });
+            }
+
+            function getAndAddToLocation_callback(callback, url, location ) {
+              getAndAddToLocation(url, location);
+              if (typeof callback === "function") {
+                callback(url);
+              }
+            }
+
+            function getAndAddToLocation(url, location) {
+              $.get(url, function(returnHTML) {
+                addToLocation(returnHTML, location);
+              });
+            }
+
+            function addToLocation(html, location) {
+              var result_location = $(document).find(location);
+              $(result_location).html(html);
+              $(result_location).css("display", "block");
+            }
+
+          });
+
+          function hideOverlay() {
+            var overlay_location = $(document).find("#overlayArea");
+            var result_location = $(document).find("#overlayContent");
+            $(result_location).html("");
+            $(overlay_location).css("display", "none");
+            $(result_location).css("display", "none");
+          }
 
